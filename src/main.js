9d9868602a0abed2d961e601f222c30027ab7cef
rarants <rarantes@inf.ufsm.br>
2022-09-17 15:01:42 -0300

    comment vue-router

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Bootstrap from "./assets/scss/custom.scss"
-import VueRouter from "vue-router";
+// import VueRouter from "vue-router";
 import router from "./router";
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 
 Vue.config.productionTip = false
 Vue.use(Bootstrap)
-Vue.use(VueRouter)
+// Vue.use(VueRouter)
 
 new Vue({
   render: h => h(App),
